7ed9741f708e3742e4bc4ef3ff8d5a9735bfcbfb
athifa123 <athifasultana1998@gmail.com>
2024-02-16 00:11:07 +0530

    outline of chatpage

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Inputbox from "./Inputbox";
 
 const Page = () => {
@@ -8,5 +9,51 @@ const Page = () => {
         </div>
             )
             }
+
+function ChatPage({children}){
+    return (
+        <div> { children} </div>
+    )
+
+}
+
+function VerticalLayout({ children}){
+    return (
+        <div> { children} </div>
+    )
+
+}
+
+function ScrollableLayout({ children}){
+    const [itemstate,setItemstate] = useState([]);
+    return (
+        <div> { children} </div>
+    )
+}
+
+function SentMessage({text}){
+    return (
+        <div> {text} </div>
+    )
+
+}
+
+function ReceivedMessage({text}){
+    return (
+        <div> {text} </div>
+    )
+
+}
+
+function HorizontalLayout({children}){
+    return (
+        <div> {children}
+        </div>
+    )
+}
+
+
+
+
 
 export default Page;
