172010878b12c15a3bdc5154b0cbd3ec177eba92
athifa123 <athifasultana1998@gmail.com>
2024-02-17 00:08:01 +0530

    implemented with mock data

--- ui/src/components/Inputbox.js
+++ ui/src/components/Inputbox.js
@@ -1,8 +1,28 @@
 const Inputbox = () => {
     return (
-        <div className="initiatechat">
-        <input/>
-        <button> Send </button>
+        <div className="initiatechat" style={{
+            "display": "flex",
+            "flex-direction": "row",
+            "width": "100%",
+            "flex-wrap": "nowrap",
+            "align-content": "center",
+            "align-items": "stretch",
+            "justify-content": " center",
+            "position": "fixed", "bottom": "0"
+        }}>
+            <input style={{
+                'width': '100%', "height"
+                    : "20%",
+                'padding': '10px',
+                'border': '1px solid purple',
+                'border-radius': '10px',
+                'background': "pink"
+            }} />
+            <button style={{
+                "background ": "green", 'padding': '10px',
+                'border': '1px solid red',
+                'border-radius': '10px',
+            }}> Send </button>
 
         </div>
 
--- ui/src/components/Page.js
+++ ui/src/components/Page.js
@@ -1,51 +1,155 @@
 import { useState } from "react";
 import Inputbox from "./Inputbox";
 
+var mock_data = [
+    {
+        "by": "me",
+        "message": "hello",
+    },
+    {
+        "by": "you",
+        "message": "hello",
+    },
+    {
+        "by": "me",
+        "message": "hey",
+    },
+    {
+        "by": "you",
+        "message": "bye",
+    },
+    {
+        "by": "me",
+        "message": "hello",
+    },
+    {
+        "by": "you",
+        "message": "hello",
+    },
+    {
+        "by": "me",
+        "message": "hey",
+    },
+    {
+        "by": "you",
+        "message": "bye",
+    },
+    {
+        "by": "me",
+        "message": "hello",
+    },
+    {
+        "by": "you",
+        "message": "hello",
+    },
+    {
+        "by": "me",
+        "message": "hey",
+    },
+    {
+        "by": "you",
+        "message": "bye",
+    },
+    {
+        "by": "me",
+        "message": "hello",
+    },
+    {
+        "by": "you",
+        "message": "hello",
+    },
+    {
+        "by": "me",
+        "message": "hey",
+    },
+    {
+        "by": "you",
+        "message": "bye",
+    },
+    {
+        "by": "me",
+        "message": "hello",
+    },
+    {
+        "by": "you",
+        "message": "hello",
+    },
+    {
+        "by": "me",
+        "message": "hey",
+    },
+    {
+        "by": "you",
+        "message": "bye",
+    },
+
+
+]
+
+
+
 const Page = () => {
     return (
-        <div> 
-            <Inputbox/> 
+        <div style={{ "background": "#eed3ac" }}>
+
+            <VerticalLayout>
+                <Inputbox />
+                <ScrollableLayout>
+                    <SentMessage />
+                    <ReceivedMessage />
+                </ScrollableLayout>
+            </VerticalLayout>
 
         </div>
-            )
-            }
+    )
+}
 
-function ChatPage({children}){
+function ChatPage({ children }) {
     return (
-        <div> { children} </div>
+        <div> {children} </div>
     )
 
 }
 
-function VerticalLayout({ children}){
+function VerticalLayout({ children }) {
     return (
-        <div> { children} </div>
+        <div style={{
+
+        }}> {children} </div>
     )
 
 }
 
-function ScrollableLayout({ children}){
-    const [itemstate,setItemstate] = useState([]);
+function ScrollableLayout({ children }) {
+    const [itemstate, setItemstate] = useState([]);
     return (
-        <div> { children} </div>
+        <div style={{ "height": "100%" }}> {mock_data.map((data) => {
+            return (data.by === "me" ? < SentMessage text={data.message} > </SentMessage> : <ReceivedMessage text={data.message} />)
+        })}</div>
     )
 }
 
-function SentMessage({text}){
+function SentMessage({ text }) {
     return (
-        <div> {text} </div>
-    )
-
-}
-
-function ReceivedMessage({text}){
-    return (
-        <div> {text} </div>
+        <div style={{ "padding": "10px", "border-radius": "5px", "display":"flex", "flexDirection":"row-reverse" }}>  <div style={{ "background": "aquamarine",
+            "padding": "7px",
+            "border-radius": "5px",
+            "font-size": "xx-small"}}> {text}  </div> </div>
     )
 
 }
 
-function HorizontalLayout({children}){
+function ReceivedMessage({ text }) {
+    return (
+        <div style={{ "padding": "10px", "border-radius": "5px", "display":"flex" }}>  <div style={{ "background": "azure",
+        "padding": "7px",
+        "border-radius": "5px",
+        "font-size": "xx-small"}}> {text}  </div> </div>
+    )
+
+}
+
+function HorizontalLayout({ children }) {
     return (
         <div> {children}
         </div>
