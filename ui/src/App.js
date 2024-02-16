1d0c76841f24af2136a88a573e87783c331d4bfa
athifa123 <athifasultana1998@gmail.com>
2024-02-16 21:02:47 +0530

    Restructured

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
 import Login from './components/Login';
-import Joinchat from './components/Joinchat';
 import Page from './components/Page';
 import { Outlet, RouterProvider , createBrowserRouter} from "react-router-dom";
+import { socket } from './socket';
+import  socketobj  from './socket';
 
 function App() {
   return (
     <div className="App">
       <Outlet/>
     
-    
-      
     </div>
   );
 }
