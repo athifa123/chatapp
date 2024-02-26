f2f7b6b48e8088ac7deb849d0dd66c16eb1988a2
athifa123 <athifasultana1998@gmail.com>
2024-02-26 23:41:20 +0530

    defined username length

--- ui/src/components/Login.js
+++ ui/src/components/Login.js
@@ -4,23 +4,34 @@ import { useState } from "react";
 
 
 const Login = () =>{
-    const [userName, setUserName] = useState("");
+    const [userName, setUserName] = useState(localStorage.getItem('username'));
+    const [isButtonEnabled, setIsButtonEnabled] = useState(false);
     const navigate = useNavigate();
 
     function onStartChat(){
-        localStorage.setItem('userName', userName);
+        localStorage.setItem('username', userName);
          navigate('/chat')
+    }
+    function onInputChange(e){
+        let value = e.target.value;
+        if (value.length >= 1) 
+        {
+            setIsButtonEnabled(true);
+        }
+        else {
+            setIsButtonEnabled(false);
+        }
+        setUserName(value);
+
     }
 
 
     return (
         <div className="container">
         <div className="startchat" >
-              <input type="text" placeholder="Please enter your name" value={userName} onChange={(e) => {
-                setUserName(e.target.value)
-              }} />  
+              <input type="text" placeholder="Please enter your name" value={userName} onChange={onInputChange} />  
             <div className="joinbutton">
-            <button  enable={useState.length>=4} onClick= { onStartChat }> Start Chatting </button>
+            <button  disabled={!isButtonEnabled} onClick= { onStartChat }> Start Chatting </button>
             </div>
         </div>
         </div>
