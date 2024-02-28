0e436eea37dc10c061c377c57d19bb6536958fb3
athifa123 <athifasultana1998@gmail.com>
2024-02-29 01:03:21 +0530

    hardcoded url

--- ui/src/socket.js
+++ ui/src/socket.js
@@ -3,4 +3,4 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? process.env.WEBSOCKET_URL : 'http://localhost:8080';
 
-export const socket = io(URL);
+export const socket = io('https://chatapp-nu-ebon.vercel.app'+'/wss-chat');
